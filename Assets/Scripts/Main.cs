7a3f47a55bfba51edad05392d5896dbd06f5321a
본인Shepherdstar <rweqtwed@gmail.com>
2026-05-18 11:44:53 +0900

    [메인 작업] 1. 메인UI , 로딩UI 이미지 및 버튼 작업

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MainUI : UImanager
+{
+
+    [SerializeField] private UImanager Button_Gamecontinue;
+    [SerializeField] private UImanager Button_Gamestart;
+    [SerializeField] private UImanager Button_Gameloading;
+    [SerializeField] private UImanager Button_GameObtion;
+    [SerializeField] private UImanager Button_GameQuit;
+
+    private void OnEnable()
+    {
+        Button_Gamecontinue.bindOnClickButtonEvent(Button_Gamecontinue);
+        Button_Gamestart.bindOnClickButtonEvent( Button_Gamestart);
+        Button_Gameloading.bindOnClickButtonEvent(Button_Gameloading);
+        Button_GameObtion.bindOnClickButtonEvent(Button_GameObtion);
+        Button_GameQuit.bindOnClickButtonEvent(Button_GameQuit);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
