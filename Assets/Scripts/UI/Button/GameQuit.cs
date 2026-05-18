09909a347498f5c14da7f55c248962c65757cd98
본인Shepherdstar <rweqtwed@gmail.com>
2026-05-18 13:15:57 +0900

    [메인 작업] 2. 메인UI  종료버튼 작업

---
+++ Assets/Scripts/UI/Button/GameQuit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GameQuit : MonoBehaviour
+{
+    // 버튼 클릭 이벤트에 바인딩되거나 외부에서 호출될 게임 종료 함수 (동사 시작)
+    public void QuitGame()
+    {
+        // 1. 유니티 에디터에서 플레이 중일 때 멈춤
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+
+        // 2. 실제 빌드된 독립 실행형 게임 종료
+        Application.Quit();
+    }
+}
--- Assets/Scripts/manager/UImanager.cs
+++ Assets/Scripts/manager/UImanager.cs
@@ -2,9 +2,12 @@
 
 public class UImanager : MonoBehaviour
 {
+    [SerializeField] Canvas Canvas_BgRoot;
+    [SerializeField] Canvas Canvas_MainRoot;
+    [SerializeField] Canvas Canvas_ContentRoot;
+    [SerializeField] Canvas Canvas_PopupRoot;
+    [SerializeField] Canvas Canvas_VeryFrontRoot;
 
-
-    bindOnClickButtonEvent
 
     void Start()
     {
