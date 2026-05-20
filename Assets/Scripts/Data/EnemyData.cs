fa1b179eba59839675613c1ed085b5e3422276d2
본인Shepherdstar <rweqtwed@gmail.com>
2026-05-20 20:44:06 +0900

    [게임 작업] 9.4 적 데이터 미미한 수정

--- Assets/Scripts/Data/EnemyData.cs
+++ Assets/Scripts/Data/EnemyData.cs
@@ -10,7 +10,7 @@ public class EnemyData
     public string Description; // 몬스터 설명 (도감용)
     public string Engine_Id; // 적의 소유 엔진
     public string Weapon_Id; // 적의 소유 무기
-    public float HP; // 적의 체력
+    public float Max_HP; // 적의 체력
     public string IconPath; // 몬스터 표기용 Icon
     public string PrefabPath; // 몬스터를 우선 프리팹으로 구현
 
--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,7 +4,7 @@ public class EnemyAI : MonoBehaviour
 {
     [Header("Enemy Live Specs")]
     [SerializeField] private float float_CurrentHP;
-    [SerializeField] private float float_HP;
+    [SerializeField] private float float_Max_HP;
     [SerializeField] private float Float_MoveSpeed;
     [SerializeField] private int Int_AttackDamage;
 
@@ -14,8 +14,8 @@ public class EnemyAI : MonoBehaviour
         EnemyData enemyMaster = GameDataManager.Instance.GetEnemyData(enemyId);
         if (enemyMaster == null) return;
 
-        float_HP = enemyMaster.HP;
-        float_CurrentHP = float_HP;
+        float_Max_HP = enemyMaster.Max_HP;
+        float_CurrentHP = float_Max_HP;
 
         // 2. 엔진 ID를 활용해 엔진 마스터 테이블 교차 검색 (속도 낚아채기)
         EngineData engineMaster = GameDataManager.Instance.GetEngineData(enemyMaster.Engine_Id);
@@ -31,6 +31,6 @@ public class EnemyAI : MonoBehaviour
             Int_AttackDamage = weaponMaster.Damage;
         }
 
-        Debug.Log($"[적 스펙 조립완료] 명칭: {enemyMaster.Name} / HP: {float_HP} / 속도: {Float_MoveSpeed} / 공격력: {Int_AttackDamage}");
+        Debug.Log($"[적 스펙 조립완료] 명칭: {enemyMaster.Name} / HP: {float_Max_HP} / 속도: {Float_MoveSpeed} / 공격력: {Int_AttackDamage}");
     }
 }
